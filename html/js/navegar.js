00dcf50e7b7b959f284ce94f8a0ac98bfae56874
Alexa Mata <alexamatafreire@gmail.com>
2025-06-03 18:57:08 +0200

    Crear lista eventos y pagina de cada evento. navegar.js ya no es necesario

--- html/js/navegar.js
+++
@@ -1,7 +0,0 @@
-function navegar(url){
-    if (document.body.classList.contains("darkMode")) {
-        document.location = url + "?darkmode=true";
-    } else {
-        document.location = url;
-    }
-}
